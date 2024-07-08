867b4b1534a3512d292bab2d63a9c15c78a809e5
mazenhaouari40 <mazenhaouari97@gmail.com>
2024-07-08 23:19:40 +0200

    third commit

--- Jenkins/scripts/deliver.bat
+++ Jenkins/scripts/deliver.bat
@@ -1,13 +1,7 @@
-@echo off
-rem Build the Angular project
+@ECHO OFF
+
 npm run ng build
+npm run ng serve > NUL & (START "" cmd /c "echo $! > .pidfile")
 
-rem Start the Angular server in a new command window
-start "" /B npm run ng serve
-
-rem Wait for a short period to allow the server to start
-timeout /T 5 /NOBREAK
-
-rem Output instructions to the user
 echo Now...
 echo Visit http://localhost:4200 to see your Node.js/Angular application in action.
