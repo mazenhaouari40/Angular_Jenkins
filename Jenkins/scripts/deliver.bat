ba2d2e1054df46d7e56f61d468ecbe48a3af5623
mazenhaouari40 <mazenhaouari97@gmail.com>
2024-07-24 10:40:33 +0200

    delete some folder

--- Jenkins/scripts/deliver.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-
-npm run ng build
-npm run ng serve > NUL & (START "" cmd /c "echo $! > .pidfile")
-
-echo Now...
-echo Visit http://localhost:4200 to see your Node.js/Angular application in action.
